baf7cff6d45d5ceb608d8356ab17f34546ee6308
HoaPQ <rubik0403@gmail.com>
2019-07-07 19:02:57 +0700

    initial commit

---
+++ Comic/Classes/Service/ApiHomeManager.swift
@@ -0,0 +1,15 @@
+//
+//  ApiHomeManager.swift
+//  Comic
+//
+//  Created by HoaPQ on 7/7/19.
+//  Copyright © 2019 HoaPQ. All rights reserved.
+//
+
+import Foundation
+
+class ApiHomeManager {
+    static let shared: ApiHomeManager = ApiHomeManager()
+    
+    
+}
